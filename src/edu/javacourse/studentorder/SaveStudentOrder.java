17460e484ceedd707d3869b01dfe51ff5787a281
Yana Davydovich <56787449+davydovichyana@users.noreply.github.com>
2022-08-19 13:29:04 +0300

    + добавлены геттеры и сеттеры в StudentOrder

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -6,17 +6,17 @@ public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so;
         so = new StudentOrder();
-        so.hFirstName = "Alex";
-        so.hLastName = "Petrov";
-        so.wFirstName = "Galina";
-        so.wLastName = "Petrova";
+        so.sethFirstName("Alex");
+        so.sethLastName("Petrov");
+        so.setwFirstName("Galina");
+        so.setwLastName("Petrova");
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
     }
-    static long saveStudentOrder(StudentOrder StudentOrder) {
+    static long saveStudentOrder(StudentOrder so) {
         long answer = 99;
-        System.out.println("Save StudentOrder 1: " + StudentOrder.hLastName);
+        System.out.println("Save StudentOrder 1: " + so.gethFirstName());
 
         return answer;
     }
