b3b271c84c1b407271eb24e480ebf324c5e980ff
DavydovichYana <luckyabsolut@gmail.com>
2022-08-20 10:17:55 +0300

    + добавлен класс-предок Person и включена сеть наследования в Adult, Child + добавлен метод buildStudentOrder() в класс SaveStudentOrder

---
+++ src/edu/javacourse/studentorder/domain/Adress.java
@@ -0,0 +1,9 @@
+package edu.javacourse.studentorder.domain;
+
+public class Adress {
+    private String postcode;
+    private String street;
+    private String building;
+    private String extension;
+    private String apartment;
+}
---
+++ src/edu/javacourse/studentorder/domain/Person.java
@@ -0,0 +1,55 @@
+package edu.javacourse.studentorder.domain;
+
+import java.time.LocalDate;
+
+public class Person {
+    protected String surName;
+    protected String givenName;
+    private String patronymic;
+    private LocalDate dateOfBirth;
+    private Adress adress;
+
+    public String getPersonString() {
+        return surName + " " + givenName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Adress getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+}
