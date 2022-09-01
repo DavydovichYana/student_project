a2e7fcbbc00fc89773a6c5c0c9ecc95cef78eeff
DavydovichYana <luckyabsolut@gmail.com>
2022-09-01 21:58:28 +0300

    + добавлены справочники паспортных и регистрационных офисов

--- src/edu/javacourse/studentorder/domain/Child.java
+++ src/edu/javacourse/studentorder/domain/Child.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Child extends Person {
     private String certificateNumber;
     private LocalDate issueDate;
-    private String issueDepartment;
+    private RegisterOffice issueDepartment;
 
     public Child(String surName, String givenName, String patronymic, LocalDate dateOfBirth) {
         super(surName, givenName, patronymic, dateOfBirth);
@@ -27,11 +27,11 @@ public class Child extends Person {
         this.issueDate = issueDate;
     }
 
-    public String getIssueDepartment() {
+    public RegisterOffice getIssueDepartment() {
         return issueDepartment;
     }
 
-    public void setIssueDepartment(String issueDepartment) {
+    public void setIssueDepartment(RegisterOffice issueDepartment) {
         this.issueDepartment = issueDepartment;
     }
 }
